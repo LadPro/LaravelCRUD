d10d9f286e231b7d5d88108a5ef3f7c719314e3b
Joel Mora <joeljmorab@gmail.com>
2024-06-26 19:12:52 -0400

    agregado controllador de nota y model de nota + migracion de nota

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/product/{id}', function () {
+//     return view('welcome');
+// });
+
+Route::get('product/{id?}', [NoteController::class, 'funtion'])->name('example.function');
