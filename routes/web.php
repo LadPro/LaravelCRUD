6301318815bfdb4793e5fd10151ece21b13009dd
Joel Mora <joeljmorab@gmail.com>
2024-06-28 11:58:17 -0400

    route resource para mas corta manera de hacer el crud

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\PostController;
 
 // Route::get('/product/{id}', function () {
 //     return view('welcome');
@@ -14,3 +15,5 @@ Route::get('note/edit/{note}', [NoteController::class, 'edit'])->name('edit');
 Route::put('note/update/{note}', [NoteController::class, 'update'])->name('update');
 Route::get('/show/{note}', [NoteController::class, 'show'])->name('show');
 Route::delete('note/destroy/{note}', [NoteController::class, 'destroy'])->name('destroy');
+
+Route::resource('/post', PostController::class);
